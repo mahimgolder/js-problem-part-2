52cc61fde65372d9fdc6d2f1e9da97012356b122
mahimgolder <mahimgolder@gmail.com>
2024-10-10 06:24:55 -0600

    resolved some code

--- max.js
+++ max.js
@@ -1,13 +1,12 @@
 // const disha =56;
 // const salman =70;
-
-
 // if(disha > salman){
 //     console.log("disha will get the strawberry");
 // }
 // else {
 //     console.log("salman will get the strawberry");
 // }
+
 // inside a function
 function getmax(num1, num2){
     if(num1 > num2){
