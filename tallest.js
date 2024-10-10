3c633b763ce21e731bd16417d9f9be3f71031675
mahimgolder <mahimgolder@gmail.com>
2024-10-09 19:14:24 -0600

    custimize the tallest.js task

--- tallest.js
+++ tallest.js
@@ -1,35 +1,15 @@
-const heights =[65, 66, 64, 72, 78, 60, 65, 66];
 
-function getMax(numbers){
-    for(const num of numbers){
-        let max =num;
-        if(num > max){
-            max = num;
+function getMax(heights){
+    let max =heights[0];
+    for(let i = 1; i < heights.length; i++ ){
+        if(heights[i] > max){
+            max = heights[i];
         }
     }
+    return max;
 }
+const heights =[65, 66, 64, 72, 78, 60, 65, 66];
 const max = getMax(heights);
 console.log('max value is :', max );
 
-
-// function giveName(){
-//     const x ="mahim"
-//     return x
-
-// }
-// const myname = giveName()
-// console.log(myname)
-
-
-
-
-// const numbers =[2, 4, 21, 5, 6, 23, 20, 9]
-
-// let biggest = numbers[0];
-// console.log('before loop:', biggest)
-// for (let i = 1; i<numbers.length; i++ ){
-//     console.log('current number:',numbers[i], 'biggest number:', biggest);
-//     if (numbers[i] > biggest){
-//         biggest = numbers[i];
-//     }
-// }
+// -----------****-----------
